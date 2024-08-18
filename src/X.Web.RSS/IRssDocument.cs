0a7546351d9eca1cf8346b56033411f7196f16d1
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2024-08-18 13:15:28 +0300

    Update IRssDocument

--- src/X.Web.RSS/IRssDocument.cs
+++ src/X.Web.RSS/IRssDocument.cs
@@ -1,9 +1,10 @@
 using JetBrains.Annotations;
+using X.Web.RSS.Structure;
 
 namespace X.Web.RSS;
 
 [PublicAPI]
-public interface IRssDocument<TRssChannel>
+public interface IRssDocument<TRssChannel> where TRssChannel : RssChannelBase
 {
     /// <summary>
     ///   Gets or sets subordinate to the 'rss' element is a single 'channel' element, 
@@ -12,7 +13,7 @@ public interface IRssDocument<TRssChannel>
     TRssChannel Channel { get; set; }
 
     /// <summary>
-    ///   Gets or sets at the top level, a RSS document is a 'rss' element, 
+    ///   Gets or sets at the top level, an RSS document is a 'rss' element, 
     ///   with a mandatory attribute called version, that specifies 
     ///   the version of RSS that the document conforms to.
     /// </summary>
